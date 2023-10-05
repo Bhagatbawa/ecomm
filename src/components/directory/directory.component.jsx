b728e12b91621ab9b01fd9e62f4ae85f3b07167a
Bhagat Singh <bhagatsingh5314@gmail.com>
2023-10-05 20:56:14 +0530

    final version of project

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,16 @@
+import CategoryItem from '../category-item/category-item.component';
+
+import './directory.styles.scss';
+const Directory = ({Categories}) => {
+    return  (
+    <div className = 'directory-container'>
+      {Categories.map((category) => (
+    <CategoryItem key={category.id} category={category} />
+    ))}
+     </div>
+    );
+}
+
+export default Directory;
+
+    
